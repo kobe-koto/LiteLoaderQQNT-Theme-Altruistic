f21f6201f7b50a592107bc4d84b60954ccb64657
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2023-07-20 17:40:52 +0800

    監聽 `src` 檔案夾內所有檔案，而不只是 `main.scss`

--- cli/dev.mjs
+++ cli/dev.mjs
@@ -1,24 +1,32 @@
 import fs from "node:fs";
 import * as sass from "sass";
 
+const WatchDir = "./src"
+
 console.info("Watching file changes.");
+const WatchList = fs.readdirSync(WatchDir);
 
-
-fs.watch("./src/main.scss", (eventType, filename) => { 
-	if (eventType === "change") {
-		console.log(`File Changed (${filename}), start to build.`);
-
-		try {
-			const { css: TargetCSS } = sass.compile("src/main.scss");
-			fs.writeFile("dist/main.css", TargetCSS, (err) => { 
-				if (err) 
-					console.log(err); 
-				else { 
-					console.log("Build done.\n\n");
-				} 
-		  	});
-		} catch (err) {
-			console.warn("build failed.");
+for (
+	let i = 0;
+	i < WatchList.length;
+	i++
+) {
+	fs.watch(`${WatchDir}/${WatchList[i]}`, (eventType, filename) => { 
+		if (eventType === "change") {
+			console.log(`${WatchDir}/${filename} Changed, building...`);
+	
+			try {
+				const { css: TargetCSS } = sass.compile("src/main.scss");
+				fs.writeFile("dist/main.css", TargetCSS, (err) => { 
+					if (err) 
+						console.log(err); 
+					else { 
+						console.log("Build done.\n\n");
+					} 
+				  });
+			} catch (err) {
+				console.warn("build failed.");
+			}
 		}
-	}
-}); 
+	}); 
+}
